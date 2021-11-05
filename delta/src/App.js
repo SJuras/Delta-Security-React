7add463b6088b2a94987b79c708b51529da79ef7
SJuras <jurasstpko@gmailcom>
2021-11-05 05:15:53 +0100

    v1.1.8 Navbar completed

--- delta/src/App.js
+++ delta/src/App.js
@@ -2,16 +2,19 @@ import React from 'react'
 import GlobalStyle from './globalStyles';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Switch } from "react-router";
+import Navbar from './components/Navbar/Navbar';
 
 // pages
 import Home from './pages/Home';
 import PricingPage from './pages/PricingPage';
 import SignupPage from './pages/SignupPage';
 
+
 function App() {
   return (
     <Router>
       <GlobalStyle />
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={SignupPage} />
