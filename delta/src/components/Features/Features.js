57b42700a3701ee6160c7a148ca937086d5195f6
SJuras <jurasstpko@gmailcom>
2021-11-05 06:09:00 +0100

    v1.1.8 features section completed

---
+++ delta/src/components/Features/Features.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Container, Section } from '../../globalStyles';
+import {
+  FeatureText,
+  FeatureTitle,
+  FeatureWrapper,
+  FeatureColumn,
+  FeatureImageWrapper,
+  FeatureName,
+  FeatureTextWrapper,
+} from './FeatureStyles.js'
+import { featuresData } from '../../data/FeaturesData';
+
+const Features = () => {
+
+  const initial = {
+		y: 40,
+		opacity: 0,
+	};
+	const animate = {
+		y: 0,
+		opacity: 1,
+	};
+
+  return(
+    <Section>
+      <Container>
+        <FeatureTextWrapper>
+          <FeatureTitle>
+            What we offer
+          </FeatureTitle>
+        </FeatureTextWrapper>
+
+        {featuresData.map((el.index)=> (
+          <FeatureColumn
+            initial={initial}
+            animate={animate}
+            transition={{ duration: 0.5 + index * 0.1 }}
+            key={index}
+          >
+          <FeatureImageWrapper className={el.imgClass}>
+								{el.icon}
+					</FeatureImageWrapper>
+						<FeatureName>{el.name}</FeatureName>
+						<FeatureText>{el.description}</FeatureText>
+          </FeatureColumn>
+        ))}
+      </Container>
+    </Section>
+  )
+};
+
+export default Features
